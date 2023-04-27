def712649c46fca8552d06c63cd3a643d0236500
Kaden Barton <22kabarton@gmail.com>
2023-04-27 10:10:07 -0500

    Added tests

--- test_app.py
+++ test_app.py
@@ -17,13 +17,17 @@ def test_index(app, client):
     assert response.status_code == 302
 
 def test_fortune(app, client):
-    pass
+    response = client.get('/fortune/')
+    assert response.status_code == 200
 
 def test_cowsay_message(app, client):
-    response = client.get('/cowsay/Hello DevOps/')
+    message = 'Hello DevOps'
+    response = client.get('/cowsay/%s/' % message)
+    assert response.status_code == 200
     assert '(oo)' in response.get_data(as_text=True)
     assert 'Hello DevOps' in response.get_data(as_text=True)
 
 def test_cowfortune(app, client):
     response = client.get('/cowfortune/')
+    assert response.status_code == 200
     assert '(oo)' in response.get_data(as_text=True)
